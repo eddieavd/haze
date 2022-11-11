7a77b0a7d0ea0b5d8fd18a8eae4cb9d60f948189
eddieavd <edhemavdagic@gmail.com>
2022-11-11 20:09:52 +0100

    updated benchmarks

--- bench/gbench_haze.cpp
+++ bench/gbench_haze.cpp
@@ -11,13 +11,15 @@ namespace haze_bench
 {
 
 
-BENCHMARK( bm_mean_blur_field      < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2, 128 )->Unit( benchmark::kMillisecond );
-BENCHMARK( bm_mean_blur_field_metal< haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2, 128 )->Unit( benchmark::kMillisecond );
-BENCHMARK( bm_mean_blur_kern_metal < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2, 128 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_mean_blur_kern       < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4,  16 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_mean_blur_kern_metal < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4, 128 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_mean_blur_field      < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4, 128 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_mean_blur_field_metal< haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4, 128 )->Unit( benchmark::kMillisecond );
 
-BENCHMARK( bm_lens_blur_field      < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2,  16 )->Unit( benchmark::kMillisecond );
-BENCHMARK( bm_lens_blur_field_metal< haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2, 128 )->Unit( benchmark::kMillisecond );
-BENCHMARK( bm_lens_blur_kern_metal < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 2, 128 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_lens_blur_kern_metal < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4, 128 )->Unit( benchmark::kMillisecond );
+//BENCHMARK( bm_lens_blur_field      < haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4,   4 )->Unit( benchmark::kMillisecond );
+BENCHMARK( bm_lens_blur_field_metal< haze::bw_pixel > )->RangeMultiplier( 2 )->Range( 4, 128 )->Unit( benchmark::kMillisecond );
+
 
 
 } // namespace haze_bench
--- bench/gbench_haze.hpp
+++ bench/gbench_haze.hpp
@@ -18,6 +18,45 @@ namespace haze_bench
 
 
 template< typename Pixel >
+auto pb
+{
+        []( haze::image< Pixel > const & lhs, haze::image< Pixel > const & rhs )
+        {
+                return ( lhs.width() * lhs.height() > rhs.width() * rhs.height() ) ? lhs : rhs;
+        }
+};
+
+/*
+template< typename Pixel >
+static void bm_segtree_img ( benchmark::State & state )
+{
+        srand( time( nullptr ) );
+
+        haze::image< Pixel > img( IMG_PATH );
+
+        for( auto _ : state )
+        {
+                npl::rq::segment_tree< haze::image< Pixel >, pb< Pixel > > segtree;
+
+                for( long long i = 0; i < state.range( 0 ); ++i )
+                {
+                        std::size_t x1 = rand() %  img.width() / 2;
+                        std::size_t x2 = rand() %  img.width() / 2 + img.width() / 2;
+                        std::size_t y1 = rand() % img.height() / 2;
+                        std::size_t y2 = rand() % img.height() / 2 + img.height() / 2;
+
+                        segtree.emplace_back( img.get_subimage( x1, y1, x2, y2 ) );
+                }
+
+                auto res = segtree.range();
+
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( res );
+        }
+}
+*/
+
+template< typename Pixel >
 static void bm_mean_blur_field ( benchmark::State & state )
 {
         haze::pixel_field< Pixel > field( IMG_PATH );
@@ -53,6 +92,20 @@ static void bm_mean_blur_field_metal ( benchmark::State & state )
         state.counters[    "iwidth" ] = field.width();
         state.counters[   "iheight" ] = field.height();
         state.counters[ "total_pix" ] = field.width() * field.height();
+}
+
+template< typename Pixel >
+static void bm_mean_blur_kern ( benchmark::State & state )
+{
+        haze::image< Pixel > img( IMG_PATH );
+
+        for( auto _ : state )
+        {
+                auto blurred = haze::mean_blur_image( img, { static_cast< std::size_t >( state.range( 0 ) ) } );
+
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( blurred );
+        }
 }
 
 template< typename Pixel >
